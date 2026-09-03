64c299412c3bfef6d8dcaf788dd3c92fb429a859
Uday Sharma <uday1.20001@gmail.com>
2026-09-03 20:07:32 +0530

    Temporarily enable debug

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -27,7 +27,7 @@ load_dotenv(BASE_DIR / '.env')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = True
 
 ALLOWED_HOSTS = os.environ.get(
     'ALLOWED_HOSTS',
